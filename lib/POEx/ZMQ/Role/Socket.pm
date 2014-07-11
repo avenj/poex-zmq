8fb6722f34239fa1a52bdd511f7a8fd356df16f0
Jon Portnoy <avenj@cobaltirc.org>
2014-07-11 15:02:16 -0400

    Role::Socket method outline, notes wrt filters
    	modified:   lib/POEx/ZMQ/Role/Socket.pm

--- lib/POEx/ZMQ/Role/Socket.pm
+++ lib/POEx/ZMQ/Role/Socket.pm
@@ -17,6 +17,9 @@ use POEx::ZMQ::Util qw/
 # FIXME grab constants directly or via an importer pkg?
 
 
+use POE;
+
+
 use Moo::Role;
 with 'POEx::ZMQ::Role::EmitEvents';
 
@@ -34,11 +37,19 @@ has type => (
   isa       => ZMQSocketType,
 );
 
+has filter => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Maybe[ InstanceOf['POE::Filter'] ],
+  builder   => sub { undef },
+);
+
 
 has zsock => (
   lazy      => 1,
   is        => 'ro',
-  isa      => ZMQSocket,
+  isa       => ZMQSocket,
+  clearer   => 'clear_zsock',  # ffi sock auto-closes in destructor
   builder   => sub {
     my ($self) = @_;
     $self->zeromq->socket( $self->type )
@@ -65,49 +76,31 @@ sub _pxz_emitter_stopped {
 }
 
 
-sub opt {
-  # FIXME sockopt getter/setter
-}
+sub getsockopt { shift->zsock->get(@_) }
+sub setsockopt { shift->zsock->set(@_) }
 
+# FIXME
+#  - Most of these should emit events.
+#  - Probably we should track connect and bind endpoints and provide a
+#    disconnect_all.
+#  - Probably there should be methods to handle the above and the frontendy
+#    methods & events can be maybe split out to a role later.
 
-sub connect {
+sub close { my $self = shift; $self->zsock->close }
+sub _px_close { $_[OBJECT]->close }
 
-}
+sub bind { my $self = shift; $self->zsock->bind($_) for @_ }
+sub _px_bind { $_[OBJECT]->bind(@_[ARG0 .. $#_]) }
 
-sub _px_connect {
+sub connect { my $self = shift; $self->zsock->connect($_) for @_ }
+sub _px_connect { $_[OBJECT]->connect(@_[ARG0 .. $#_]) }
 
-}
-
-
-sub bind {
-
-}
-
-sub _px_bind {
-
-}
-
-
-sub disconnect {
-
-}
-
-sub _px_disconnect {
-
-}
-
-
-sub close {
-
-}
-
-sub _px_close {
-
-}
-
+sub disconnect { my $self = shift; $self->zsock->disconnect($_) for @_ }
+sub _px_disconnect { $_[OBJECT]->disconnect(@_[ARG0 .. $#_]) }
 
 sub send {
-
+  # FIXME do we want a separate send_multipart or handle it here?
+  # FIXME if $self->filter, use POE::Filter serializer iface
 }
 
 sub _px_send {
