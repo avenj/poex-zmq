4df88d72d67bdc4f743079e321b4b83df8be7b9d
Jon Portnoy <avenj@cobaltirc.org>
2014-07-16 00:59:46 -0400

    POEx::ZMQ::FFI->find_soname / ->get_version
    	modified:   lib/POEx/ZMQ/FFI.pm

---
+++ lib/POEx/ZMQ/FFI.pm
@@ -0,0 +1,72 @@
+package POEx::ZMQ::FFI;
+
+use v5.10;
+use Carp;
+use strictures 1;
+
+use FFI::Raw;
+
+use List::Objects::WithUtils;
+
+use Try::Tiny;
+
+
+
+sub find_soname {
+  my ($class) = @_;
+
+  state $search = array( qw/
+    libzmq.so libzmq.so.3
+    libzmq.dylib libzmq.3.dylib
+  / );
+
+  my $soname;
+  SEARCH: for my $maybe ($search->all) {
+    try {
+      FFI::Raw->new(
+        $soname, zmq_version =>
+          FFI::Raw::void,
+          FFI::Raw::ptr,
+          FFI::Raw::ptr,
+          FFI::Raw::ptr,
+      );
+      $soname = $maybe;
+    };
+    last SEARCH if defined $soname
+  }
+
+  croak "Failed to locate a suitable libzmq in your linker's search path"
+    unless defined $soname;
+
+  my $vers = $class->get_version($soname);
+  unless ($vers->major >= 3) {
+    my $vstr = join '.', $vers->major, $vers->minor, $vers->patch;
+    croak "This library requires ZeroMQ 3+ but you only have $vstr"
+  }
+  
+  $soname
+}
+
+sub get_version {
+  my ($class, $soname) = @_;
+  $soname //= $class->find_soname;
+
+  my $zmq_vers = FFI::Raw->new(
+    $soname, zmq_version =>
+      FFI::Raw::void,
+      FFI::Raw::ptr,  # -> major
+      FFI::Raw::ptr,  # -> minor 
+      FFI::Raw::ptr,  # -> patch
+  );
+  my ($maj, $min, $pat) = map {; pack 'i!', $_ } (0, 0, 0);
+  $zmq_vers->(
+    map {; unpack 'L!', pack 'P', $_ } ($maj, $min, $pat)
+  );
+  ($maj, $min, $pat) = map {; unpack 'i!', $_ } ($maj, $min, $pat);
+  hash(
+    major => $maj, minor => $min, patch => $pat
+  )->inflate
+}
+
+
+1;
