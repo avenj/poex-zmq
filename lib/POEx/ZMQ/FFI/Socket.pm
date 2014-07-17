f58fbcbd140f1a9c93f27d270bfdf55b810c8473
Jon Portnoy <avenj@cobaltirc.org>
2014-07-17 02:28:13 -0400

    FFI::Socket->_ffi builder
    	modified:   lib/POEx/ZMQ/FFI/Socket.pm

--- lib/POEx/ZMQ/FFI/Socket.pm
+++ lib/POEx/ZMQ/FFI/Socket.pm
@@ -3,15 +3,18 @@ package POEx::ZMQ::FFI::Socket;
 use Carp;
 use strictures 1;
 
+use IO::Handle ();
+
 use Types::Standard  -types;
 use POEx::ZMQ::Types -types;
 
+use POEx::ZMQ::Constants -all;
+use POEx::ZMQ::FFI;
+
+use FFI::Raw;
+
 
 sub OPTVAL_MAXLEN () { 256 }
-
-sub T_INT { 0 }
-sub T_BIN { 1 }
-sub T_STR { 2 }
 
 
 use Moo; use MooX::late;
@@ -42,7 +45,128 @@ has _ffi => (
   builder   => sub {
     my $soname = shift->soname;
     POEx::ZMQ::FFI::Callable->new(
-      # FIXME
+      zmq_socket => FFI::Raw->new(
+        $soname, zmq_socket =>
+          FFI::Raw::ptr,  # <- socket ptr
+          FFI::Raw::ptr,  # -> context ptr
+          FFI::Raw::int,  # -> socket type
+      ),
+
+      zmq_getsockopt => FFI::Raw->new(
+        $soname, zmq_getsockopt =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::int,  # -> opt (constant)
+          FFI::Raw::ptr,  # -> opt value ptr
+          FFI::Raw::ptr,  # -> value len ptr
+      ),
+
+      zmq_setsockopt_int => FFI::Raw->new(
+        $soname, zmq_setsockopt =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::int,  # -> opt (constant)
+          FFI::Raw::ptr,  # -> opt value ptr (int)
+          FFI::Raw::int,  # -> opt value len
+      ),
+      zmq_setsockopt_str => FFI::Raw->new(
+        $soname, zmq_setsockopt =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::int,  # -> opt (constant)
+          FFI::Raw::str,  # -> opt value ptr (str)
+          FFI::Raw::int,  # -> opt value len
+      ),
+
+      zmq_connect => FFI::Raw->new(
+        $soname, zmq_connect =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::str,  # -> endpoint str
+      ),
+
+      zmq_disconnect => FFI::Raw->new(
+        $soname, zmq_disconnect =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::str,  # -> endpoint str
+      ),
+
+      zmq_bind => FFI::Raw->new(
+        $soname, zmq_bind =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::str,  # -> endpoint str
+      ),
+
+      zmq_unbind => FFI::Raw->new(
+        $soname, zmq_unbind =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::str,  # -> endpoint str
+      ),
+
+      zmq_msg_init => FFI::Raw->new(
+        $soname, zmq_msg_init =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+      ),
+
+      zmq_msg_init_size => FFI::Raw->new(
+        $soname, zmq_msg_init_size =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+          FFI::Raw::int,  # -> len
+      ),
+
+      zmq_msg_size => FFI::Raw->new(
+        $soname, zmq_msg_size =>
+          FFI::Raw::int,  # <- len
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+      ),
+
+      zmq_msg_data => FFI::Raw->new(
+        $soname, zmq_msg_data =>
+          FFI::Raw::ptr,  # <- msg data ptr
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+      ),
+
+      zmq_msg_close => FFI::Raw->new(
+        $soname, zmq_msg_close =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+      ),
+
+      zmq_msg_recv => FFI::Raw->new(
+        $soname, zmq_msg_recv =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> zmq_msg_t ptr
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::int,  # -> flags
+      ),
+
+      zmq_send => FFI::Raw->new(
+        $soname, zmq_send =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+          FFI::Raw::str,  # -> msg
+          FFI::Raw::int,  # -> len
+          FFI::Raw::int,  # -> flags
+      ),
+
+      zmq_close => FFI::Raw->new(
+        $soname, zmq_close =>
+          FFI::Raw::int,  # <- rc
+          FFI::Raw::ptr,  # -> socket ptr
+      ),
+
+      memcpy => FFI::Raw->new(
+        undef, memcpy =>
+          FFI::Raw::ptr,  # <- dest ptr
+          FFI::Raw::ptr,  # -> dest buf ptr
+          FFI::Raw::ptr,  # -> src
+          FFI::Raw::int,  # -> len
+      ),
     )
   },
 );
@@ -92,7 +216,7 @@ sub set_sock_opt {
 
 
 sub get_handle {
-  require IO::Handle;
+  my ($self) = @_;
   my $fno = $self->get_sock_opt( ZMQ_FD, 'int' );
   IO::Handle->new_from_fd( $fno, 'r' )
 }
