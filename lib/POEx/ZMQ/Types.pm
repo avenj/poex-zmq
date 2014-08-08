618fac252db89c057dd3acba9eff1fd9271fd990
Jon Portnoy <avenj@cobaltirc.org>
2014-08-08 12:15:19 -0400

    Types improvements; ZMQSocket[$type], use ZMQSocket for async socks
    	modified:   dist.ini
    	modified:   lib/POEx/ZMQ/Socket.pm
    	modified:   lib/POEx/ZMQ/Types.pm
    	new file:   t/types.t

--- lib/POEx/ZMQ/Types.pm
+++ lib/POEx/ZMQ/Types.pm
@@ -6,13 +6,25 @@ use Type::Library   -base;
 use Type::Utils     -all;
 use Types::Standard -types;
 
+use Module::Runtime ();
+
 use POEx::ZMQ::Constants ();
 
 declare ZMQContext =>
   as InstanceOf['POEx::ZMQ::FFI::Context'];
 
-declare ZMQSocket =>
+declare ZMQSocketBackend =>
   as InstanceOf['POEx::ZMQ::FFI::Socket'];
+
+declare ZMQSocket =>
+  as InstanceOf['POEx::ZMQ::Socket'],
+  constraint_generator => sub {
+    my $want_ztype = shift;
+    if (my $sub = POEx::ZMQ::Constants->can($want_ztype)) {
+      $want_ztype = $sub->()
+    }
+    sub { $_->type == $want_ztype }
+  };
 
 declare ZMQSocketType => as Int;
 coerce  ZMQSocketType => 
@@ -41,6 +53,14 @@ L<Type::Tiny>-based types for L<POEx::ZMQ>.
 A L<POEx::ZMQ::FFI::Context> object.
 
 =head2 ZMQSocket
+
+A L<POEx::ZMQ::Socket> object.
+
+=head2 ZMQSocket[`a]
+
+A L</ZMQSocket> can be parameterized with a given L</ZMQSocketType>.
+
+=head2 ZMQSocketBackend
 
 A L<POEx::ZMQ::FFI::Socket> object.
 
